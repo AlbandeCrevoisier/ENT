63f104cf5c38915415cf90048903a27906e263de
Alban de Crevoisier d'Hurbache <albandecrevoisier@gmail.com>
2018-03-25 19:14:09 +0200

    Add Register landing component.

---
+++ handy-sitter/src/components/Register.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>Bienvenue sur Handy-Sitter</h1>
+    <h2>Garde d'enfants handicapés</h2>
+    <h3>S'enregistrer</h3>
+	<b-button variant='primary' to='/register-parent'>Parent</b-button>
+	<b-button variant='primary' to='/register-sitter'>Sitter</b-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'register',
+  data () {
+    return {
+    }
+  }
+}
+</script>
