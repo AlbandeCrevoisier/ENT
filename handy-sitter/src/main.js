9cc60f0a37840d9963cd5c31c9e0818384e676c0
Alban de Crevoisier d'Hurbache <albandecrevoisier@gmail.com>
2018-03-25 18:41:44 +0200

    Import file uploader.

--- handy-sitter/src/main.js
+++ handy-sitter/src/main.js
@@ -4,10 +4,12 @@ import Vue from 'vue'
 import BootstrapVue from 'bootstrap-vue'
 import App from './App'
 import router from './router'
+import VueClip from 'vue-clip'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.use(BootstrapVue)
+Vue.use(VueClip)
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
