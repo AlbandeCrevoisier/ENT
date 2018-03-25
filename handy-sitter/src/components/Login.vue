c7a34766c5d98cc175f4a3932b201f2ce366ccc4
Alban de Crevoisier d'Hurbache <albandecrevoisier@gmail.com>
2018-03-25 19:47:55 +0200

    Add skel form for login.

--- handy-sitter/src/components/Login.vue
+++ handy-sitter/src/components/Login.vue
@@ -3,6 +3,13 @@
     <h1>Bienvenue sur Handy-Sitter</h1>
     <h2>Garde d'enfants handicapés</h2>
     <h3>Se connecter</h3>
+    <b-form inline>
+      <label class='sr-only' for='eMailIn'>e-Mail</label>
+	  <b-form-input id='eMailIn' type='email' placeholder='e-mail' />
+      <label class='sr-only' for='passIn'>Password</label>
+	  <b-form-input id='passIn' type='password' placeholder='mot de passe' />
+		<b-button variant='primary'>Se connecter</b-button>
+    </b-form>
   </div>
 </template>
 
