6fe952726d889a6469127d46e16bc369cabfcdb3
Alban de Crevoisier d'Hurbache <albandecrevoisier@gmail.com>
2018-03-25 18:26:49 +0200

    Add landing page to routes.

--- handy-sitter/src/router/index.js
+++ handy-sitter/src/router/index.js
@@ -8,8 +8,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Hello',
-      component: Hello
+      name: 'Landing',
+      component: Landing 
     },
 	{
 	  path: '/login',
