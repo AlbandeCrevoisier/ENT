0a300b355dc2e0d3e613f478a0f63d273784b256
Alban de Crevoisier d'Hurbache <albandecrevoisier@gmail.com>
2018-03-25 18:27:10 +0200

    Add langing page component.

---
+++ handy-sitter/src/components/Landing.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1>Bienvenue sur Handy-Sitter</h1>
+  <h2>Garde d'enfants handicapés</h2>
+  <b-button to='/Login'>Se connecter</b-button>
+  <b-button to='/Register'>S'enregistrer</b-button>
+</template>
+
+<script>
+export default {
+  name: 'landing',
+  data () {
+    return {
+  }
+}
+</script>
