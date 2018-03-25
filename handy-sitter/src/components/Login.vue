7fce9d972d09d4fdedbac19579f094ba347a3eff
Alban de Crevoisier d'Hurbache <albandecrevoisier@gmail.com>
2018-03-25 19:14:55 +0200

    Add login component.

---
+++ handy-sitter/src/components/Login.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>Bienvenue sur Handy-Sitter</h1>
+    <h2>Garde d'enfants handicapés</h2>
+    <h3>Se connecter</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'login',
+  data () {
+    return {
+    }
+  }
+}
+</script>
