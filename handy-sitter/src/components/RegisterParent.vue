05d49a538e0e242fea258dd13c5669c54492a816
Alban de Crevoisier d'Hurbache <albandecrevoisier@gmail.com>
2018-03-29 19:47:56 +0200

    Add button to add child from parent registration.

--- handy-sitter/src/components/RegisterParent.vue
+++ handy-sitter/src/components/RegisterParent.vue
@@ -4,6 +4,7 @@
     <h2>Garde d'enfants handicapés</h2>
     <h3>S'enregistrer</h3>
     <h4>Parent</h4>
+    <b-button variant='primary' @click='addChild'>Enregistrer un enfant</b-button>
   </div>
 </template>
 
@@ -12,6 +13,11 @@ export default {
   name: 'register-parent',
   data () {
     return {
+    }
+  },
+  methods: {
+    addChild: function () {
+      // Add a child component
     }
   }
 }
