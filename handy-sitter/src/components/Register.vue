29523f878fc6fc52c0ae16c8196811b0718e86cc
Alban de Crevoisier d'Hurbache <albandecrevoisier@gmail.com>
2018-03-29 19:30:49 +0200

    Correct tab shenanigans in pasting to Register.

--- handy-sitter/src/components/Register.vue
+++ handy-sitter/src/components/Register.vue
@@ -7,32 +7,32 @@
       <b-form-row>
         <b-col>
           <label class='sr-only' for='nameIn'>Name</label>
-		  <b-form-input id='nameIn' placeholder='Prénom' />
+          <b-form-input id='nameIn' placeholder='Prénom' />
         </b-col>
       </b-form-row>
       <b-form-row>
         <b-col>
           <label class='sr-only' for='surnameIn'>Surame</label>
-		  <b-form-input id='surnameIn' placeholder='Nom' />
-		</b-col>
+          <b-form-input id='surnameIn' placeholder='Nom' />
+        </b-col>
       </b-form-row>
       <b-form-row>
         <b-col>
           <label class='sr-only' for='eMailIn'>e-Mail</label>
-		  <b-form-input id='eMailIn' type='email' placeholder='e-Mail' />
-		</b-col>
+          <b-form-input id='eMailIn' type='email' placeholder='e-Mail' />
+        </b-col>
       </b-form-row>
       <b-form-row>
         <b-col>
           <label class='sr-only' for='passIn'>Password</label>
-		  <b-form-input id='passIn' type='password' placeholder='Mot de passe' />
-		</b-col>
+          <b-form-input id='passIn' type='password' placeholder='Mot de passe' />
+        </b-col>
       </b-form-row>
       <b-form-row>
         <b-col>
           <label class='sr-only' for='passConfirm'>Confirm Password</label>
-		  <b-form-input id='passConfirm' type='password' placeholder='Confirmez le mot de passe' />
-		</b-col>
+          <b-form-input id='passConfirm' type='password' placeholder='Confirmez le mot de passe' />
+        </b-col>
       </b-form-row>
     </b-container>
     <b-button variant='primary' to='/register-parent'>Parent</b-button>
