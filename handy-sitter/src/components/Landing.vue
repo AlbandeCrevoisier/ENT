de8028236654ea8c7f9044183ae10b3042c66762
Alban de Crevoisier d'Hurbache <albandecrevoisier@gmail.com>
2018-03-29 17:34:52 +0200

    Add description to landing page.

--- handy-sitter/src/components/Landing.vue
+++ handy-sitter/src/components/Landing.vue
@@ -4,6 +4,14 @@
     <h2>Garde d'enfants handicapés</h2>
     <b-button variant='primary' to='/login'>Se connecter</b-button>
     <b-button variant='primary' to='/register'>S'enregistrer</b-button>
+    <p>
+      Handy-Sitter est une plateforme de baby-sitting pour enfants handicapés.
+      Notre but est de mettre en relation des baby-sitters sensibilisés au
+      handicap avec des parents d'enfants handicapés pour allier la souplesse
+      du baby-sitting avec la nécessité d'être formé pour pouvoir garder un
+      enfant ayant des besoins particuliers du fait d'un handicap. Nous pouvons
+      réaliser cela en recrutant des baby-sitters parmi les étudiants de santé.
+    </p>
   </div>
 </template>
 
