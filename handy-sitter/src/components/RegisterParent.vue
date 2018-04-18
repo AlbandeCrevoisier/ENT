0c203088984a2d902ed7c727880cd03b3bb0a3eb
Alban de Crevoisier <albandecrevoisier@gmail.com>
2018-04-18 18:43:09 +0200

    Add rows and columns organisation to parent registration.

--- handy-sitter/src/components/RegisterParent.vue
+++ handy-sitter/src/components/RegisterParent.vue
@@ -2,14 +2,21 @@
   <div>
     <h3>S'enregistrer</h3>
     <h4>Parent</h4>
-    <RegisterParentChild
-      v-for='child in children'
-      :key='children.indexOf(child)'
-      :childIdx='children.indexOf(child)'
-      :parentIdx='parentIdx' />
-    <b-button variant='primary' @click='addChild'>Ajouter un enfant</b-button>
-    <b-button variant='primary' @click='delChild'>Supprimer un enfant</b-button>
-    <b-button variant='primary' to='/'>Valider</b-button>
+    <b-container>
+      <b-row><b-col>
+        <RegisterParentChild v-for='child in children'
+                             :key='children.indexOf(child)'
+                             :childIdx='children.indexOf(child)'
+                             :parentIdx='parentIdx' />
+      </b-col></b-row>
+      <b-row><b-col>
+        <b-button variant='primary' @click='addChild'>Ajouter un enfant</b-button>
+        <b-button variant='primary' @click='delChild'>Supprimer un enfant</b-button>
+      </b-col></b-row>
+      <b-row><b-col>
+        <b-button variant='primary' to='/'>Valider</b-button>
+      </b-col></b-row>
+    </b-container>
   </div>
 </template>
 
