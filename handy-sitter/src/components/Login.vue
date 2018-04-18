ced2024d2121608039b8ffc3bf302a0dee21f9e3
Alban de Crevoisier <albandecrevoisier@gmail.com>
2018-04-18 17:26:27 +0200

    Add local data to login, & clean up code.

--- handy-sitter/src/components/Login.vue
+++ handy-sitter/src/components/Login.vue
@@ -1,12 +1,20 @@
 <template>
   <div>
     <h3>Se connecter</h3>
-    <b-form inline>
+    <b-form @submit='canLogin' inline>
       <label class='sr-only' for='eMailIn'>e-Mail</label>
-	  <b-form-input id='eMailIn' type='email' placeholder='e-Mail' />
+      <b-form-input id='eMailIn'
+                    type='email'
+                    placeholder='e-Mail'
+                    v-model='email'
+                    required />
       <label class='sr-only' for='passIn'>Password</label>
-	  <b-form-input id='passIn' type='password' placeholder='Mot de passe' />
-		<b-button variant='primary'>Login</b-button>
+      <b-form-input id='passIn'
+                    type='password'
+                    placeholder='Mot de passe'
+                    v-model='pass'
+                    required />
+      <b-button variant='submit' >Login</b-button>
     </b-form>
   </div>
 </template>
@@ -16,6 +24,13 @@ export default {
   name: 'login',
   data () {
     return {
+      email: '',
+      pass: ''
+    }
+  },
+  methods: {
+    canLogin () {
+      this.$router.push('/home')
     }
   }
 }
--- handy-sitter/src/components/Register.vue
+++ handy-sitter/src/components/Register.vue
@@ -1,40 +1,63 @@
 <template>
   <div>
     <h3>S'enregistrer</h3>
-    <b-container>
-      <b-form-row>
-        <b-col>
-          <label class='sr-only' for='nameIn'>Name</label>
-          <b-form-input v-model='infos.name' id='nameIn' placeholder='Prénom' />
-        </b-col>
-      </b-form-row>
-      <b-form-row>
-        <b-col>
-          <label class='sr-only' for='surnameIn'>Surame</label>
-          <b-form-input v-model='infos.surname' id='surnameIn' placeholder='Nom' />
-        </b-col>
-      </b-form-row>
-      <b-form-row>
-        <b-col>
-          <label class='sr-only' for='eMailIn'>e-Mail</label>
-          <b-form-input v-model='infos.email' id='eMailIn' type='email' placeholder='e-Mail' />
-        </b-col>
-      </b-form-row>
-      <b-form-row>
-        <b-col>
-          <label class='sr-only' for='passIn'>Password</label>
-          <b-form-input id='passIn' type='password' placeholder='Mot de passe' />
-        </b-col>
-      </b-form-row>
-      <b-form-row>
-        <b-col>
-          <label class='sr-only' for='passConfirm'>Confirm Password</label>
-          <b-form-input id='passConfirm' type='password' placeholder='Confirmez le mot de passe' />
-        </b-col>
-      </b-form-row>
-    </b-container>
-    <b-button variant='primary' @click='registerParent'>Parent</b-button>
-    <b-button variant='primary' @click='registerSitter'>Sitter</b-button>
+    <b-form @submit='sub'>
+      <b-container>
+        <b-form-row>
+          <b-col>
+            <label class='sr-only' for='nameIn'>Name</label>
+            <b-form-input id='nameIn'
+                          placeholder='Prénom'
+                          v-model='infos.name'
+                          required />
+          </b-col>
+        </b-form-row>
+        <b-form-row>
+          <b-col>
+            <label class='sr-only' for='surnameIn'>Surame</label>
+            <b-form-input id='surnameIn'
+                          placeholder='Nom'
+                          v-model='infos.surname'
+                          required />
+          </b-col>
+        </b-form-row>
+        <b-form-row>
+          <b-col>
+            <label class='sr-only' for='eMailIn'>e-Mail</label>
+            <b-form-input id='eMailIn'
+                          type='email'
+                          placeholder='e-Mail'
+                          v-model='infos.email'
+                          required />
+          </b-col>
+        </b-form-row>
+        <b-form-row>
+          <b-col>
+            <label class='sr-only' for='passIn'>Password</label>
+            <b-form-input id='passIn' type='password' placeholder='Mot de passe' />
+          </b-col>
+        </b-form-row>
+        <b-form-row>
+          <b-col>
+            <label class='sr-only' for='passConfirm'>Confirm Password</label>
+            <b-form-input id='passConfirm' type='password' placeholder='Confirmez le mot de passe' />
+          </b-col>
+        </b-form-row>
+        <b-form-row>
+          <b-col>
+            <label class='sr-only' for='posPick'>Parent or Sitter</label>
+            <b-form-radio-group id='posPick'
+                                buttons
+                                v-model='infos.pos'
+                                name='posPick'>
+              <b-form-radio value='p'>parent</b-form-radio>
+              <b-form-radio value='s'>sitter</b-form-radio>
+            </b-form-radio-group>
+          </b-col>
+        </b-form-row>
+      </b-container>
+      <b-button type='submit' variant='primary'>Suivant</b-button>
+    </b-form>
   </div>
 </template>
 
@@ -46,18 +69,20 @@ export default {
       infos: {
         name: '',
         surname: '',
-        email: ''
+        email: '',
+        pos: ''
       }
     }
   },
   methods: {
-    registerParent () {
-      this.$store.commit('addParent', {...this.infos, children: []})
-      this.$router.push('/register-parent/' + this.$store.getters.pIdx)
-    },
-    registerSitter () {
-      this.$store.commit('addSitter', {...this.infos})
-      this.$router.push('/register-sitter/' + this.$store.getters.sIdx)
+    sub () {
+      if (this.pos === 'p') {
+        this.$store.commit('addParent', {...this.infos, children: []})
+        this.$router.push('/register-parent/' + this.$store.getters.pIdx)
+      } else {
+        this.$store.commit('addSitter', {...this.infos})
+        this.$router.push('/register-sitter/' + this.$store.getters.sIdx)
+      }
     }
   }
 }
